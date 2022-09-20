703727369d49cbad96c6e64aee9712cd4724f145
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2022-09-21 01:28:46 +0600

    Update aes.cpp

--- IS/AES/aes.cpp
+++ IS/AES/aes.cpp
@@ -4,8 +4,8 @@ using namespace std;
 const int N=1024;
 string message;
 string keyString;
-char key[16];
-
+unsigned char key[16];
+unsigned char state[16];
 void padMessage(){
     int len=message.size();
     int newLen=len;
@@ -34,90 +34,95 @@ void convertToHex(){
     }
 }
 
-void expandKey(char expandedKey[]){
-    	for (int i = 0; i < 16; i++) {
+void expandKey(unsigned char expandedKey[]){
+    for (int i = 0; i < 16; i++) {
 		expandedKey[i] = key[i];
 	}
 
-	int bytesGenerated = 16; 
-	int rconIteration = 1; 
+	int bytesGenerated = 16; //done so far
 	unsigned char tmpCore[4]; 
 
 	while (bytesGenerated < 176) {
 		
 		for (int i = 0; i < 4; i++) {
-			tmpCore[i] = expandedKey[i + bytesGenerated - 4];
+			tmpCore[i] = expandedKey[i - 4 + bytesGenerated];//prothom 4 ta temporary save
 		}
-
+        //cout<<bytesGenerated<<endl;
 		// Perform the core once for each 16 byte key
 		if (bytesGenerated % 16 == 0) {
-			KeyExpansionCore(tmpCore, rconIteration++);
+			KeyExpansionCore(tmpCore, (bytesGenerated/16)-1);
+            //cout<<"Roundkey number:  "<<(bytesGenerated/16)-1<<endl;
 		}
 
-		for (unsigned char a = 0; a < 4; a++) {
-			expandedKey[bytesGenerated] = expandedKey[bytesGenerated - 16] ^ tmpCore[a];//4 ghor ager tar sathe xor 1 ghor ager ta xor
-			bytesGenerated++;
+		for (int a = 0; a < 4; a++) {
+			expandedKey[bytesGenerated+a] = tmpCore[a]^expandedKey[bytesGenerated - 16+ a] ;//4 ghor ager tar sathe xor 1 ghor ager ta xor
 		}
+        bytesGenerated+=4;
 
 	}
 }
 
 
-
-string encryption(string message, int index, char expandedKey[]){
-    char state[16];
+void encryption(string message, int index, unsigned char expandedKey[]){
+    
     int j = index;
     for(int i=0;i<16;i++){
         state[i]=message[j++];//copying first 16 blocks into state
     }
-    // for(int i=0; i<16; ++i)
-    //     cout << hex << state[i];
+   
 
     int nR=10;//number of rounds
-    char roundKey[16];
+    unsigned char roundKey[16];
     addRoundKey(state, expandedKey);//initial addroundkey
-    for(int i=0;i<nR-1;i++){
+    for(int i=0;i<9;i++){
             subBytes(state);
             shiftRows(state);
             mixColumn(state);
             getRoundKey(roundKey,expandedKey,i);
-            addRoundKey(state,roundKey);
+            addRoundKey(state,roundKey); 
     }
     subBytes(state);
 	shiftRows(state);
     getRoundKey(roundKey,expandedKey,9);
-	addRoundKey(state, key);
-    
-    string encStream;
-
-    for(int i=0;i<16;i++){
-        encStream+=state[i];
-    }
-    return encStream;
-
+	addRoundKey(state, roundKey);
 }
 
 int main(){
     printf("Enter message: ");
     getline(cin,message);
     padMessage();
+    printf("Message in hex:\n");
+    for (int i = 0; i < message.length(); i++) {
+		cout << hex << (int) message[i];
+		cout << " ";
+	}
+    cout<<endl;
     printf("Enter the 128 bit key: ");
     getline(cin,keyString);
     convertToHex();
+    printf("Key string in hex:\n");
+    for (int i = 0; i < keyString.length(); i++) {
+		cout << hex << (int) keyString[i];
+		cout << " ";
+	}
+    cout<<endl;
     /*for(int i=0; i<16; ++i)
         cout << hex << (int)key[i];*/
     //input done
    
-    char expandedKey[176];
+    unsigned char expandedKey[176];
     expandKey(expandedKey);
+//     printf("Expanded key:\n");
 //    for(int i=0; i<176; ++i)
 //         cout << hex << (int)expandedKey[i];
-    string encryptedMessage;
-    for(int i=0;i<message.length();i+=16){//16 kore kore nibo
-        encryptedMessage += encryption(message, i, expandedKey);
-    }
+//     cout<<endl;
     printf("Encrypted message: ");
-
-    cout<<encryptedMessage<<endl;
+    for(int i=0;i<message.length();i+=16){
+        encryption(message, i, expandedKey);
+        for (int j = 0; j < 16; j++) {
+		    cout << hex << (int) state[j];
+		    cout << " ";
+	    }
+    }
 }
 
