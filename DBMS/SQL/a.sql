150cd53ede299c2cfaf9cb86c4ee4679370a90dc
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2022-10-15 01:03:42 +0600

    Update a.sql

--- DBMS/SQL/a.sql
+++ DBMS/SQL/a.sql
@@ -4,3 +4,4 @@
 --ORDER BY DEPARTMENT_ID, SALARY DESC;
 --SELECT FIRST_NAME||' '||LAST_NAME||'''s' || ' SALARY WITH COMMISSION IS '|| (SALARY+NVL(COMMISSION_PCT,0))*12 "ANNUAL SALARY" FROM EMPLOYEES WHERE EMPLOYEE_ID=&EMPLOYEE_IN;
 SELECT &INPUT FROM EMPLOYEES ORDER BY &&INPUT;  
+select ((select count(city) from station)-(select count(distinct city) from station)) from dual;
